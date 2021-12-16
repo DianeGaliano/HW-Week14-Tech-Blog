27d1219b6ab6199fc042987c3ab639b2ecd77c5a
Diane Galiano <dianegaliano09@gmail.com>
2021-12-15 20:58:27 -0600

    fixing the homeRoutes file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,64 +1,85 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models')
-const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
+const { Post, User, Comment} = require('../models');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all Posts and JOIN with user data
-    const postData = await Post.findAll({
+router.get('/', (req, res) => {
+  Post.findAll(
+    {
+      attributes: ['id', 'title', 'content', 'created_at'],
       include: [
         {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/post/:id', async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
           model: Comment,
+          attributes: [ 'id', 'comment_text', 'post_id', 'user_id', 'create_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
         },
-      ],
-    });
-
-    const posts = postData.get({ plain: true });
-
-    res.render('post', {
-      ...posts,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+        {
+          model: User,
+          attributes: [ 'username'],
+        },
+      ]
+    }
+  )
+  .then((postData) => {
+    const posts = postData.map((Post) => Post.get({plain: true}));
+    res.render('homepage', {posts, loggedIn: req.session.loggedIn});
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json;
+  });
 });
 
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
+router.get('login', (req, res) => {
+  if (res.session.loggedIn) {
+    res.redirect('/');
     return;
   }
-
   res.render('login');
 });
 
+router.get('/Post/:id', (req, res) => {
+  Post.findOne( {
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+    include: [
+      {
+        model: Comment, 
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+  .then((postData) => {
+    if (!postData) {
+      res.status(404).json ({
+        message: 'Post with this id not found.'
+      });
+    }
+    const posts = postData.get({ plain: true });
+    res.render('singlePost', { posts, loggedIn: req.session.loggedIn });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
+router.get('signUp', (req, res) => {
+  res.render('signUp');
+});
 
-module.exports = router;
+router.get('/postComments', (req, res) => {
+  Post.
+})
