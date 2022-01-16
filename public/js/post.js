b8ab46b1a92f5ecbf53ad126dc65af8c970d28ea
Diane Galiano <dianegaliano09@gmail.com>
2022-01-15 23:11:52 -0600

    created a new post and save post js file

---
+++ public/js/post.js
@@ -0,0 +1,19 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="postTitle"]').value;
+    const content = document.querySelector('#postContent').value;
+    const response = await fetch ('api/post', {
+        method: 'POST',
+        body: JSON.stringify ({title, content}),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    }else 
+    alert(response.status);
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
